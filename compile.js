f5c450927652575d119d2cb6afe1f3d72b1241e2
srehan1 <syed.rehan@shoppertrak.com>
2023-03-30 19:06:46 +0530

    Initial commit

---
+++ compile.js
@@ -0,0 +1,27 @@
+// compile code will go here
+
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf-8");
+
+const input = {
+  language: "Solidity",
+  sources: {
+    "Inbox.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+module.exports = output.contracts["Inbox.sol"]["Inbox"];
---
+++ test/Inbox.test.js
@@ -0,0 +1,47 @@
+// contract test code will go here
+
+const assert = require("assert");
+const ganache = require("ganache");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+const { abi, evm } = require("../compile");
+
+let accounts;
+let inbox;
+const DEFAULT_MESSAGE = "Hi there!";
+
+beforeEach(async () => {
+  // --- Get list of all accounts ---
+  /*
+  // Instead of promise, use async-await
+  web3.eth
+    .getAccounts()
+    .then((fetchedAccounts) => console.log(fetchedAccounts));
+    */
+  accounts = await web3.eth.getAccounts();
+
+  // --- Use an account to deploy the contract ---
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: evm.bytecode.object,
+      arguments: [DEFAULT_MESSAGE],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("When contract `Inbox` is deployed", () => {
+  it("Deployment should be success.", () => {
+    assert.ok(inbox._address);
+  });
+
+  it("Has default value set.", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, DEFAULT_MESSAGE);
+  });
+
+  it("Shold be able to set new message.", async () => {
+    await inbox.methods.setMessage("Bye bye !!!").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Bye bye !!!");
+  });
+});
